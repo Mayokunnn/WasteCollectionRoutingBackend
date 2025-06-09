88cd79933fca6b5524c20b38d5b173305d836f48
Areola Kelvin Mayokun <kareola960@gmail.com>
2025-06-09 09:11:15 +0100

    Hosting

--- src/database/connection.py
+++ src/database/connection.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from src.database.models import Base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://mayokun:areolam25@localhost:5432/waste_management"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.wfpqjovuietnphgthojn:mac2024@aws-0-eu-north-1.pooler.supabase.com:5432/postgres"
 
 # Create engine and session
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
